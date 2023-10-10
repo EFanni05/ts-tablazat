180e3040fd22ec4ade1970696696ac66394f104b
Fannichan <elekes.fanni2005@gmail.com>
2023-10-10 14:03:51 +0200

    ReadIn

--- tablazat/src/main.ts
+++ tablazat/src/main.ts
@@ -1,2 +1,28 @@
 import '../node_modules/bootstrap/scss/style.css'
 import { Product } from './Product'
+let items:Product[] = []
+
+function Add(){
+  
+}
+
+function ReadIn(){
+  try{
+    const name = (document.getElementById('name') as HTMLInputElement).value
+    const price = parseInt((document.getElementById('price') as HTMLInputElement).value)
+    const code = (document.getElementById('code') as HTMLInputElement).value
+    const product : Product = new Product(name, price, code)
+    items.push(product)
+    Add()
+  } catch(e){
+    if(e instanceof Error){
+      console.log(e.message)
+    }
+  }
+}
+
+function init(){
+  document.getElementById('addtbl')!.addEventListener('click', ReadIn)
+}
+
+document.addEventListener('DOMContentLoaded', init)
